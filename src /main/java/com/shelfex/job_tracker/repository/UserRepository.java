5ed10dabe4a99ab0d97958ff0e80417477ec5973
Deepak Khokhar <125497098+RdjatDeepak@users.noreply.github.com>
2025-06-19 21:02:36 +0530

    Update UserRepository.java

--- src /main/java/com/shelfex/job_tracker/repository/UserRepository.java
+++ src /main/java/com/shelfex/job_tracker/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.shelfex.job_tracker.repository;
 
 import com.shelfex.job_tracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
